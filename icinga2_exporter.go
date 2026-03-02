5cc6f90a3eddaf6eb7750e874e09fcc4a55c3546
Markus Opolka <7090372+martialblog@users.noreply.github.com>
2026-03-02 13:48:36 +0100

    Add proper shutdown of HTTP server

--- icinga2_exporter.go
+++ icinga2_exporter.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
-	"log"
 	"log/slog"
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/martialblog/icinga2-exporter/internal/collector"
@@ -76,7 +79,7 @@ func main() {
 	flag.Parse()
 
 	if cliVersion {
-		fmt.Printf("icinga-exporter version: %s\n", version)
+		fmt.Printf("icinga-exporter version: %s\n", buildVersion())
 		os.Exit(0)
 	}
 
@@ -96,6 +99,9 @@ func main() {
 		Level: logLevel,
 	}))
 
+	// In general, listen to gosec. But it this case, I don't think someone
+	// is going to overflow the uint TTL for the cache lifetime.
+	// nolint:gosec
 	cacheTTL := time.Duration(cliCacheTTL) * time.Second
 
 	config := icinga.Config{
@@ -123,6 +129,18 @@ func main() {
 		prometheus.MustRegister(collector.NewIcinga2APICollector(c, logger))
 	}
 
+	// Create a central context to propagate a shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	srv := &http.Server{
+		Addr:              cliListenAddress,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       3 * time.Second,
+		WriteTimeout:      3 * time.Second,
+		IdleTimeout:       5 * time.Second,
+	}
+
 	http.Handle(cliMetricsPath, promhttp.Handler())
 	//nolint:errcheck
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
@@ -136,7 +154,29 @@ func main() {
 			</html>`))
 	})
 
-	log.Printf("Version: %s", buildVersion())
-	log.Printf("Listening on address: %s", cliListenAddress)
-	log.Fatal(http.ListenAndServe(cliListenAddress, nil))
+	go func() {
+		slog.Info("Listening on address", "port", cliListenAddress, "version", version, "commit", commit)
+		// nolint:noinlineerr
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("HTTP server error", "error", err.Error())
+		}
+
+		slog.Info("Received Shutdown. Stopped serving new connections.")
+	}()
+
+	// The signal channel will block until we registered signals are received.
+	// We will then use a context with a timeout to shutdown the application gracefully.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(ctx, 5*time.Second)
+	defer shutdownCancel()
+
+	// We are using Shutdown() with a timeout to gracefully
+	// shut down the server without interrupting any active connections.
+	// nolint:noinlineerr
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("HTTP shutdown error", "error", err.Error())
+	}
 }
